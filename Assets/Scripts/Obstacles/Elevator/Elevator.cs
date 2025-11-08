260a03c944c587bda55e40c5aaaea2dc53c390c4
yanos1 <yanos241999@gmail.com>
2025-11-08 20:37:00 +0200

    new level added. last level?

--- Assets/Scripts/Obstacles/Elevator/Elevator.cs
+++ Assets/Scripts/Obstacles/Elevator/Elevator.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections;
+using Interfaces;
 using UnityEngine;
 using Obstacles.Buttons;
 using Player;
@@ -6,36 +8,58 @@ using Player;
 namespace Obstacles.Elevator
 {
     [RequireComponent(typeof(Collider2D))] // Or Collider if 3D
-    public class Elevator : MonoBehaviour, IActivate
+    public class Elevator : MonoBehaviour, IActivate, IReset
     {
         [Header("References")]
         [SerializeField] private Transform endingPoint; // Target top position
 
-        [Header("Settings")] [SerializeField] private float moveSpeed;
+        [Header("Settings")]
+        [SerializeField] private float moveSpeed = 2f;
 
         private Vector3 _startPoint;
         private Vector3 _targetPoint;
         private bool _isMoving = false;
         private bool _isActivated = false;
-      
+
+        private Vector3 _lastPosition;
+        private Vector2 _currentVelocity;
+
+        private PlayerManager _currentPlayer; // The player currently on the elevator
 
         private void Awake()
         {
             _startPoint = transform.position;
             _targetPoint = _startPoint;
+            _lastPosition = _startPoint;
         }
 
         private void FixedUpdate()
         {
-            if (!_isMoving)
-                return;
-
-            transform.position = Vector3.MoveTowards(transform.position, _targetPoint, moveSpeed * Time.fixedDeltaTime);
-
-            if (Vector3.Distance(transform.position, _targetPoint) < 0.01f)
+            // Move the elevator
+            if (_isMoving)
             {
-                transform.position = _targetPoint;
-                _isMoving = false;
+                transform.position = Vector3.MoveTowards(transform.position, _targetPoint, moveSpeed * Time.fixedDeltaTime);
+
+                if (Vector3.Distance(transform.position, _targetPoint) < 0.01f)
+                {
+                    transform.position = _targetPoint;
+                    _isMoving = false;
+                }
+            }
+
+            // Calculate elevator velocity
+            _currentVelocity = ((Vector2)(transform.position - _lastPosition)) / Time.fixedDeltaTime;
+            _lastPosition = transform.position;
+
+            // Apply elevator velocity to the player
+            if (_currentPlayer != null)
+            {
+                Rigidbody2D playerRb = _currentPlayer.GetComponent<Rigidbody2D>();
+                if (playerRb != null)
+                {
+                    // Only move player if grounded or on elevator
+                    playerRb.position += _currentVelocity * Time.fixedDeltaTime;
+                }
             }
         }
 
@@ -53,27 +77,46 @@ namespace Obstacles.Elevator
             if (!_isActivated) return;
             _isActivated = false;
 
+            // Stop elevator immediately
+            _isMoving = false;
+
+            // Start coroutine to return after 1 second
+            StartCoroutine(ReturnAfterDelay(1f));
+        }
+
+        private IEnumerator ReturnAfterDelay(float delay)
+        {
+            // Wait for the delay
+            yield return new WaitForSeconds(delay);
+
+            // Start moving back
             _targetPoint = _startPoint;
             _isMoving = true;
         }
+
+
 
         private void OnCollisionEnter2D(Collision2D other)
         {
             var playerManager = other.gameObject.GetComponent<PlayerManager>();
             if (playerManager != null)
             {
-                playerManager.transform.SetParent(transform);
+                _currentPlayer = playerManager;
             }
         }
 
         private void OnCollisionExit2D(Collision2D other)
         {
             var playerManager = other.gameObject.GetComponent<PlayerManager>();
-            if (playerManager != null)
+            if (playerManager != null && _currentPlayer == playerManager)
             {
-                playerManager.transform.SetParent(null);
-                DontDestroyOnLoad(playerManager);
+                _currentPlayer = null;
             }
+        }
+
+        public void OnReset()
+        {
+            transform.position = _startPoint;
         }
     }
 }
